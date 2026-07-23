ad019fe4fd55c8a11e7b25824f2f98e40245261b
Vikasmnu <84653707+vikasmnu@users.noreply.github.com>
2026-07-23 17:46:39 +0530

    updated game

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
@@ -17,20 +21,50 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+            
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
+            }
         })
 })
+
 /**
  * Challenge:
  * 
- * Place each of the cards we draw into its respective card-slot
- * Hint: consider using element.children in the DOM instead of
- * giving each card-slot its own unique ID
+ * Keep score! Every time the computer wins a hand, add a point to
+ * the computer's score. Do the same for every time you win a hand.
+ * If it's a war, no points are awarded to either player. If it's 
+ * a war (same card values), no one is awarded points.
  * 
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/children
+ * Display the computer's score above the top card, display your
+ * own score BELOW the bottom card.
+ * 
+ * Track the scores in a global variable defined at the top of this file
+ * 
+ * Add to the global scores inside the `determineCardWinner` function below.
  */
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "War!"
+    }
+}
+
